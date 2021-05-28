4651a19e3d3ff8f27ca284c9ce43748a78572e52
agumy <nov13sh@gmail.com>
2021-05-28 21:28:30 +0900

    use tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center text-2xl">
+      Hello, Next.js!
+    </div>
+  );
+};
+
+export default Home;

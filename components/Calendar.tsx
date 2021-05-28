6527772b63246f0aebea11957329ab92eab1eba6
agumy <nov13sh@gmail.com>
2021-05-28 22:33:36 +0900

    display weekday

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,9 +1,23 @@
+import setDay from "date-fns/setDay";
+
 export const Calendar = () => {
   return (
-    <div className="flex flex-wrap h-full w-full p-4">
-      {[...Array(30).keys()].map(() => (
-        <div className="w-1/7 " />
-      ))}
+    <div className="flex flex-col h-full w-full p-4">
+      <div className="w-full h-7 flex">
+        {[...Array(7).keys()].map((n) => (
+          <div className="w-1/7 flex items-center justify-center border-gray-300 border border-r-0 last:border-r">
+            {setDay(new Date(), n).toLocaleDateString(
+              global.navigator?.language,
+              { weekday: "short" }
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-wrap h-full w-full">
+        {[...Array(30).keys()].map(() => (
+          <div className="w-1/7 " />
+        ))}
+      </div>
     </div>
   );
 };

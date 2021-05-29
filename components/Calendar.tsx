2465c453c9901db5259ecc7e733975412596f30a
agumy <nov13sh@gmail.com>
2021-05-29 11:53:37 +0900

    improve calendar UI

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -5,7 +5,7 @@ export const Calendar = () => {
     <div className="flex flex-col h-full w-full p-4">
       <div className="w-full h-7 flex">
         {[...Array(7).keys()].map((n) => (
-          <div className="w-1/7 flex items-center justify-center border-gray-300 border border-r-0 last:border-r">
+          <div className="w-1/7 flex items-center justify-center font-bold border-gray-300 border border-r-0 border-b-0 last:border-r">
             {setDay(new Date(), n).toLocaleDateString(
               global.navigator?.language,
               { weekday: "short" }
@@ -13,9 +13,13 @@ export const Calendar = () => {
           </div>
         ))}
       </div>
-      <div className="flex flex-wrap h-full w-full">
-        {[...Array(30).keys()].map(() => (
-          <div className="w-1/7 " />
+      <div className="flex flex-col h-full w-full">
+        {[...Array(5).keys()].map(() => (
+          <div className="flex h-full w-full">
+            {[...Array(7).keys()].map(() => (
+              <div className="w-1/7 h-full border-gray-300 border-l border-b last:border-r" />
+            ))}
+          </div>
         ))}
       </div>
     </div>

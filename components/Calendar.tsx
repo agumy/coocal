4d221431cceb83dc3f0657e59832905d2ba50786
agumy <nov13sh@gmail.com>
2021-05-29 14:52:17 +0900

    display month calendar

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,33 +1,42 @@
-import lastDayOfMonth from "date-fns/lastDayOfMonth";
-import setDate from "date-fns/setDate";
-import setDay from "date-fns/setDay";
-import setMonth from "date-fns/setMonth";
-import subDays from "date-fns/subDays";
+import {
+  addDays,
+  differenceInDays,
+  lastDayOfMonth,
+  lastDayOfWeek,
+  setDay,
+  startOfMonth,
+  startOfWeek,
+} from "date-fns";
+
+const arrayChunk = <T extends any[]>([...array]: T, size = 1): T[] => {
+  return array.reduce(
+    (acc, value, index) =>
+      index % size ? acc : [...acc, array.slice(index, index + size)],
+    []
+  );
+};
 
 export const Calendar = () => {
-  // const [month, setMonth] = useState(new Date().getMonth());
-
   const monthCalendar = (() => {
     const today = new Date();
-    const first = setDate(today, 1);
-    const weekdayOfFirst = first.getDay();
-    const lastMonth = setMonth(today, today.getMonth() - 1);
-    const lastdayOfMonth = lastDayOfMonth(lastMonth);
+    const first = startOfMonth(today);
+    const last = lastDayOfMonth(today);
+    const startDateOfWeek = startOfWeek(first);
+    const lastDateOfWeek = lastDayOfWeek(last);
+    const diff = differenceInDays(lastDateOfWeek, startDateOfWeek);
 
-    const finalWeekOfLastMonth = [...Array(weekdayOfFirst).keys()]
-      .map((n) => subDays(lastdayOfMonth, n))
-      .reverse();
+    const calendar = [...Array(diff + 1).keys()].map((n) =>
+      addDays(startDateOfWeek, n)
+    );
 
-    const firstWeek = [...finalWeekOfLastMonth, first];
-
-    console.log(firstWeek);
+    return arrayChunk(calendar, 7);
   })();
 
   return (
     <div className="flex flex-col h-full w-full p-4">
       <div className="w-full h-7 flex">
         {[...Array(7).keys()].map((n) => (
-          <div className="w-1/7 flex items-center justify-center font-bold border-gray-300 border border-r-0 border-b-0 last:border-r">
+          <div className="w-1/7 flex items-center justify-center text-gray-400 border-gray-300 border border-r-0 border-b-0 last:border-r">
             {setDay(new Date(), n).toLocaleDateString(
               global.navigator?.language,
               { weekday: "short" }
@@ -36,10 +45,14 @@ export const Calendar = () => {
         ))}
       </div>
       <div className="flex flex-col h-full w-full">
-        {[...Array(5).keys()].map(() => (
+        {monthCalendar.map((weekCalendar) => (
           <div className="flex h-full w-full">
-            {[...Array(7).keys()].map(() => (
-              <div className="w-1/7 h-full border-gray-300 border-l border-b last:border-r" />
+            {weekCalendar.map((date) => (
+              <div className="flex items-start justify-center w-1/7 p-1 h-full text-gray-500 border-gray-300 border-l border-b last:border-r">
+                {date.toLocaleDateString("en-US", {
+                  day: "numeric",
+                })}
+              </div>
             ))}
           </div>
         ))}

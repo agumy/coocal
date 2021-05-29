834c647eb275661413a94b50da6697ec1882ff9d
agumy <nov13sh@gmail.com>
2021-05-29 12:20:28 +0900

    calc firstweek for calendar

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,6 +1,28 @@
+import lastDayOfMonth from "date-fns/lastDayOfMonth";
+import setDate from "date-fns/setDate";
 import setDay from "date-fns/setDay";
+import setMonth from "date-fns/setMonth";
+import subDays from "date-fns/subDays";
 
 export const Calendar = () => {
+  // const [month, setMonth] = useState(new Date().getMonth());
+
+  const monthCalendar = (() => {
+    const today = new Date();
+    const first = setDate(today, 1);
+    const weekdayOfFirst = first.getDay();
+    const lastMonth = setMonth(today, today.getMonth() - 1);
+    const lastdayOfMonth = lastDayOfMonth(lastMonth);
+
+    const finalWeekOfLastMonth = [...Array(weekdayOfFirst).keys()]
+      .map((n) => subDays(lastdayOfMonth, n))
+      .reverse();
+
+    const firstWeek = [...finalWeekOfLastMonth, first];
+
+    console.log(firstWeek);
+  })();
+
   return (
     <div className="flex flex-col h-full w-full p-4">
       <div className="w-full h-7 flex">

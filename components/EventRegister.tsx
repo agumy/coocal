1bfa6b71f0732fc2f67ca1e7a4278ff0b91b1041
agumy <nov13sh@gmail.com>
2021-06-06 22:59:16 +0900

    fetch ingredient

--- components/EventRegister.tsx
+++ components/EventRegister.tsx
@@ -1,4 +1,5 @@
 import { SyntheticEvent, useCallback, useState } from "react";
+import { useQuery } from "react-query";
 
 const useUrl = (initialState: string = "") => {
   const [url, __setUrl] = useState(initialState);
@@ -12,6 +13,20 @@ const useUrl = (initialState: string = "") => {
 
 export const EventRegister = () => {
   const [url, setUrl] = useUrl();
+
+  const { isLoading, error, data, refetch } = useQuery(
+    `ingredients-${url}`,
+    () =>
+      fetch(`http://localhost:3000/api/ingredient?url=${url}`).then((res) =>
+        res.json()
+      ),
+    { enabled: false }
+  );
+
+  const onClick = useCallback(() => {
+    refetch();
+  }, [refetch]);
+
   return (
     <div className="flex flex-col h-24 w-full">
       <div className="flex w-full h-11">
@@ -26,7 +41,7 @@ export const EventRegister = () => {
       <div className="flex justify-end items-end h-full">
         <button
           className="rounded border px-3 py-1 bg-blue-500 text-gray-100"
-          // onClick={onClick}
+          onClick={onClick}
         >
           登録
         </button>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "tailwindcss/tailwind.css";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="h-screen w-screen flex flex-col text-gray-600">
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen w-screen flex flex-col text-gray-600">
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
   );
 }
 

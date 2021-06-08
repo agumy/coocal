fdae939ed5a3b32e36c1d8c6bb32d5a387b1a504
agumy <nov13sh@gmail.com>
2021-06-08 23:08:34 +0900

    improve UI

--- components/EventRegister.tsx
+++ components/EventRegister.tsx
@@ -29,17 +29,17 @@ export const EventRegister = () => {
   return (
     <div className="flex flex-col h-full w-full gap-2">
       <div className="flex w-full h-11">
-        <input type="text" placeholder="Title" className="p-2 w-full text-lg" />
+        <input type="text" placeholder="献立" className="p-2 w-full text-lg" />
       </div>
       <div className="h-full w-full text-xs">
         <div className="w-full flex items-center gap-2 h-6">
           <input
             type="text"
             placeholder="URL"
-            className="p-2 h-full w-full border"
+            className="p-2 h-full w-full border rounded"
           />
-          <button className="border px-2 h-full rounded text-gray-500">
-            Import
+          <button className="border px-2 h-full rounded text-gray-500 whitespace-nowrap">
+            レシピからインポート
           </button>
         </div>
       </div>
@@ -50,19 +50,22 @@ export const EventRegister = () => {
               <input
                 className="py-1 px-2 w-full"
                 type="text"
-                placeholder="ingredient"
+                placeholder="材料"
               />
               <input
                 className="py-1 px-2 w-16"
                 type="text"
-                placeholder="amount"
+                placeholder="数量"
               />
             </li>
           ))}
         </ol>
       </div>
-      <div className="flex justify-end items-end h-full">
-        <button className="rounded border px-3 py-1 bg-blue-500 text-gray-100">
+      <div className="flex justify-between items-center h-full">
+        <button className="rounded border px-3 py-1 text-gray-500">
+          材料を追加
+        </button>
+        <button className="rounded px-3 py-1 bg-blue-500 text-gray-100">
           登録
         </button>
       </div>

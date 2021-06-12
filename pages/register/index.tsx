c5b5fae3a56bbced9c874a2f63482d8a405e5a43
agumy <nov13sh@gmail.com>
2021-06-12 17:02:36 +0900

    add register page

---
+++ pages/register/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Register: NextPage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      こんにちは
+    </div>
+  );
+};
+
+export default Register;

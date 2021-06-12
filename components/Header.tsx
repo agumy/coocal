ac2a22d9556290cb3814a92e188571c6b165e85a
agumy <nov13sh@gmail.com>
2021-06-12 17:02:29 +0900

    split header

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import { useCallback } from "react";
+import Link from "next/link";
+import { auth, googleAuthProvider } from "../firebase";
+
+export const Header = () => {
+  const login = useCallback(() => {
+    auth.signInWithRedirect(googleAuthProvider).then((res) => console.log(res));
+  }, []);
+
+  const logout = useCallback(async () => {
+    await auth.signOut();
+  }, [auth]);
+
+  return (
+    <header className="h-32 w-full bg-blue-200 flex items-center justify-between px-4">
+      <div>
+        <Link href="/">
+          <h1 className="text-gray-500 cursor-pointer">Coocal</h1>
+        </Link>
+      </div>
+      <div className="flex items-center justify-end gap-2">
+        <Link href="/register">
+          <span className="border border-gray-500 rounded py-1 px-2 text-gray-500 cursor-pointer">
+            登録
+          </span>
+        </Link>
+        <button
+          onClick={logout}
+          className="border border-gray-500 rounded py-1 px-2 text-gray-500"
+        >
+          ログアウト
+        </button>
+      </div>
+    </header>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "tailwindcss/tailwind.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Header } from "../components/Header";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="h-screen w-screen flex flex-col text-gray-600">
+      <div className="h-screen w-screen flex flex-col text-gray-600 bg-blue-100">
+        <Header />
         <Component {...pageProps} />
       </div>
     </QueryClientProvider>

3988acd9a55b6b5c3694378e070cfd90d134b293
agumy <nov13sh@gmail.com>
2021-06-13 16:58:29 +0900

    handle error

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,6 +1,10 @@
 import { NextPage } from "next";
+import { useRouter } from "next/dist/client/router";
+import { useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
 import { auth } from "../../firebase";
+import { generateMessageByCode } from "../../helper/generateMessageByCode";
+import Spinner from "react-bootstrap/Spinner";
 
 const Login: NextPage = () => {
   const {
@@ -11,23 +15,40 @@ const Login: NextPage = () => {
     reValidateMode: "onBlur",
   });
 
-  const signIn = handleSubmit(async (data) => {
-    if (data.email && data.password) {
-      try {
-        await auth.signInWithEmailAndPassword(data.email, data.password);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  });
+  const [error, setError] = useState("");
+
+  const [isLoading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const signIn = useMemo(
+    () =>
+      handleSubmit(async (data) => {
+        if (data.email && data.password) {
+          setLoading(true);
+          try {
+            await auth.signInWithEmailAndPassword(data.email, data.password);
+            router.push("/");
+          } catch (error) {
+            setError(generateMessageByCode(error.code));
+          } finally {
+            setLoading(false);
+          }
+        }
+      }),
+    [auth, handleSubmit]
+  );
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
+    <div className="w-full h-full flex flex-col items-center justify-center relative">
       <form
         onSubmit={signIn}
         className="w-1/3 h-auto border rounded-lg shadow bg-gray-100 flex flex-col justify-center p-4 gap-3 my-3"
       >
         <h1 className="text-2xl">ログイン</h1>
+        {Boolean(error) && (
+          <span className="text-red-600 text-sm">{error}</span>
+        )}
         <label className="flex flex-col gap-1">
           <span className="text-lg">メールアドレス</span>
           <input
@@ -48,10 +69,6 @@ const Login: NextPage = () => {
             className="py-1 px-2 rounded"
             {...register("password", {
               required: "パスワードを入力してください。",
-              minLength: {
-                value: 8,
-                message: "パスワードは８文字以上で入力してください。",
-              },
             })}
           />
           <div className="text-red-600 text-sm h-3">
@@ -60,11 +77,21 @@ const Login: NextPage = () => {
         </label>
 
         <div className="mt-2 flex justify-end">
-          <input
-            className="py-1 px-3 border rounded bg-white"
-            type="submit"
-            value="ログイン"
-          />
+          <button type="submit" className="py-1 px-3 border rounded bg-white">
+            {isLoading ? (
+              <>
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+              </>
+            ) : (
+              "ログイン"
+            )}
+          </button>
         </div>
       </form>
     </div>

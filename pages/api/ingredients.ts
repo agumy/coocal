44ad065dce9a19cfb469d74595f634335a06c3e8
agumy <nov13sh@gmail.com>
2021-06-16 22:10:47 +0900

    add api

---
+++ pages/api/ingredients.ts
@@ -0,0 +1,42 @@
+import jsdom from "jsdom";
+import fetch from "node-fetch";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { url } = req.query;
+
+  try {
+    if (typeof url === "string") {
+      const response = await fetch(url);
+      const html = await response.text();
+
+      const dom = new jsdom.JSDOM(html);
+      const { document } = dom.window;
+      const title = document
+        .querySelector("h1")
+        ?.textContent?.replace("レシピ・作り方", "")
+        .trim();
+
+      const ingredientList = Array.from(
+        document.querySelector(".ingredient-list")?.children || []
+      ).filter((ingredient) => !ingredient.classList.contains("group-title"));
+
+      const jsonIngredientList = ingredientList.map((e) => {
+        const ingredient = e.children;
+        return {
+          name: ingredient[0].textContent?.trim(),
+          amount: ingredient[1].textContent?.trim(),
+        };
+      });
+
+      const result = {
+        title,
+        ingredientList: jsonIngredientList,
+      };
+
+      res.json(result);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};

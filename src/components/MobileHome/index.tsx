447b9716d230effd874fbd0cdb9556a13902da9a
agumy <nov13sh@gmail.com>
2021-07-01 23:00:55 +0900

    calendar height:100

--- src/components/MobileHome/index.tsx
+++ src/components/MobileHome/index.tsx
@@ -5,7 +5,7 @@ export const MobileHome = () => {
   return (
     <div className="w-full h-full flex flex-col">
       <header className="w-full h-24 sticky top-0 border bg-white"></header>
-      <main className="flex w-full flex-wrap">
+      <main className="flex h-full w-full flex-wrap">
         {monthlyCalendar.map((weekly) =>
           weekly.map((date) => (
             <div className="w-1/7 flex justify-center pt-2">

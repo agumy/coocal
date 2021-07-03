02e34fd1420c3be323e781619560b260e89465ba
agumy <nov13sh@gmail.com>
2021-07-03 23:39:25 +0900

    display menu for target date

--- src/pages/menus/index.tsx
+++ src/pages/menus/index.tsx
@@ -1,6 +1,10 @@
 import { GetServerSideProps, NextPage } from "next";
 import { useUserAgent } from "next-useragent";
+import parse from "date-fns/parse";
+import { useRouter } from "next/dist/client/router";
 import { useMemo } from "react";
+import { useMonthlyMenus } from "../../hooks/useMonthlyMenus";
+import { Spin } from "antd";
 
 type Props = {
   ua: string;
@@ -11,6 +15,24 @@ const Menus: NextPage<Props> = ({ ua }) => {
     return useUserAgent(global.navigator?.userAgent ?? ua);
   }, [ua]);
 
+  const router = useRouter();
+  const targetDate = useMemo(() => {
+    if (typeof router.query.date === "string") {
+      return router.query.date;
+    }
+    return "";
+  }, [router]);
+
+  const { data, isLoading } = useMonthlyMenus(
+    parse(targetDate, "yyyy-MM-dd", new Date())
+  );
+
+  const menus = useMemo(() => {
+    if (data) {
+      return data.filter((menu) => menu.date === targetDate);
+    }
+  }, [data]);
+
   return (
     <>
       {!device.isMobile ? (
@@ -18,7 +40,13 @@ const Menus: NextPage<Props> = ({ ua }) => {
       ) : (
         <div className="flex flex-col h-full">
           <header className="h-16 border-b"></header>
-          <div></div>
+          <div className="h-full w-full">
+            {isLoading ? (
+              <Spin />
+            ) : (
+              menus?.map((menu) => <div>{menu.name}</div>)
+            )}
+          </div>
         </div>
       )}
     </>

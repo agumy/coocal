968baef4392725cefd518305ba888ee8f5e48d7e
agumy <nov13sh@gmail.com>
2021-07-04 14:38:03 +0900

    display header for day page

--- src/pages/menus/index.tsx
+++ src/pages/menus/index.tsx
@@ -5,10 +5,15 @@ import { useRouter } from "next/dist/client/router";
 import { useMemo } from "react";
 import { useMonthlyMenus } from "../../hooks/useMonthlyMenus";
 import { Spin } from "antd";
+import classNames from "classnames";
+import { useMonthlyCalendar } from "../../hooks/useMonthlyCalendar";
+import isSameDay from "date-fns/isSameDay";
 
 type Props = {
   ua: string;
 };
+
+const Weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const Menus: NextPage<Props> = ({ ua }) => {
   const device = useMemo(() => {
@@ -16,22 +21,32 @@ const Menus: NextPage<Props> = ({ ua }) => {
   }, [ua]);
 
   const router = useRouter();
-  const targetDate = useMemo(() => {
+  const targetDateString = useMemo(() => {
     if (typeof router.query.date === "string") {
       return router.query.date;
     }
     return "";
   }, [router]);
 
-  const { data, isLoading } = useMonthlyMenus(
-    parse(targetDate, "yyyy-MM-dd", new Date())
-  );
+  const targetDate = useMemo(() => {
+    return parse(targetDateString, "yyyy-MM-dd", new Date());
+  }, [targetDateString]);
+
+  const { data, isLoading } = useMonthlyMenus(targetDate);
 
   const menus = useMemo(() => {
     if (data) {
-      return data.filter((menu) => menu.date === targetDate);
+      return data.filter((menu) => menu.date === targetDateString);
     }
   }, [data]);
+
+  const [calendar] = useMonthlyCalendar(targetDate);
+
+  const weekly = useMemo(() => {
+    return calendar.find((week) =>
+      week.some((date) => isSameDay(date, targetDate))
+    );
+  }, [calendar, targetDate]);
 
   return (
     <>
@@ -40,13 +55,40 @@ const Menus: NextPage<Props> = ({ ua }) => {
       ) : (
         <div className="flex flex-col h-full">
           <header className="h-16 border-b"></header>
-          <div className="h-full w-full">
-            {isLoading ? (
-              <Spin />
-            ) : (
-              menus?.map((menu) => <div>{menu.name}</div>)
-            )}
-          </div>
+          <main className="h-full w-full flex flex-col">
+            <header className="flex flex-col w-full h-24 border-b">
+              <div className="font-bold flex justify-center items-center text-lg">
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "long",
+                }).format(targetDate)}
+              </div>
+              <div className="flex items-end h-full pb-1">
+                {[...Array(7).keys()].map((n) => (
+                  <div className="w-1/7 flex flex-col justify-center items-center">
+                    <div>{Weekday[n]}</div>
+                    {weekly && (
+                      <div
+                        className={classNames({
+                          "text-blue-400":
+                            weekly[n]?.getDate() === targetDate.getDate(),
+                        })}
+                      >
+                        {weekly[n]?.getDate()}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </header>
+            <div className="h-full w-full">
+              {isLoading ? (
+                <Spin />
+              ) : (
+                menus?.map((menu) => <div key={menu.id}>{menu.name}</div>)
+              )}
+            </div>
+          </main>
         </div>
       )}
     </>

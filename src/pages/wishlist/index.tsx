197cb11181c8b68fb1c49fff3a402fd1e019419c
agumy <nov13sh@gmail.com>
2021-07-12 21:18:29 +0900

    fix wishlist

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -1,10 +1,34 @@
 import { NextPage } from "next";
+import { Result } from "antd";
+
 import { MobileContainer } from "../../components/mobile/containers/MobileContainer";
 import { AddMenuButton } from "../../components/mobile/organisms/AddMenuButton";
+
+const useWishList = () => {
+  return [{ id: "1" }];
+};
 const Wishlist: NextPage = () => {
+  const wishList = useWishList();
+
   return (
     <MobileContainer>
-      <main className="h-full w-full"></main>
+      <main className="h-full w-full">
+        {wishList.length ? (
+          wishList.map((e) => (
+            <div
+              className="w-full h-16 flex flex-col justify-center items-start border-b p-3"
+              key={e.id}
+            >
+              <span className="font-bold">test</span>
+              <span className="text-sm text-gray-400">
+                {/* author: {menu.author} */}
+              </span>
+            </div>
+          ))
+        ) : (
+          <Result title="献立が登録されていません" />
+        )}
+      </main>
       <AddMenuButton />
     </MobileContainer>
   );

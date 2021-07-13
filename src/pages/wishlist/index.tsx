302e506709c0092f4d17ff691f3635d32706a58b
agumy <nov13sh@gmail.com>
2021-07-13 22:33:40 +0900

    add wishlist/menu

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import { Result } from "antd";
 
 import { MobileContainer } from "../../components/mobile/containers/MobileContainer";
@@ -12,16 +13,18 @@ const Wishlist: NextPage = () => {
     <MobileContainer>
       <main className="h-full w-full">
         {data ? (
-          data.map((e) => (
-            <div
-              className="w-full h-16 flex flex-col justify-center items-start border-b p-3"
-              key={e.id}
-            >
-              <span className="font-bold">{e.name}</span>
-              <span className="text-sm text-gray-400">
-                {/* author: {menu.author} */}
-              </span>
-            </div>
+          data.map((menu) => (
+            <Link key={menu.id} href={`wishlist/menu?id=${menu.id}`}>
+              <div
+                className="w-full h-16 flex flex-col justify-center items-start border-b p-3"
+                key={menu.id}
+              >
+                <span className="font-bold">{menu.name}</span>
+                <span className="text-sm text-gray-400">
+                  author: {menu.author}
+                </span>
+              </div>
+            </Link>
           ))
         ) : (
           <Result title="献立が登録されていません" />
---
+++ src/pages/wishlist/menu.tsx
@@ -0,0 +1,82 @@
+import { useMemo } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useUserAgent } from "next-useragent";
+import { useRouter } from "next/dist/client/router";
+import { Spin } from "antd";
+import { useUpdateMenu } from "../../hooks/useUpdateMenu";
+import { format } from "../../helper/calendar";
+import { MobileContainer } from "../../components/mobile/containers/MobileContainer";
+import { WeekNavigator } from "../../components/mobile/organisms/WeekNavigator";
+import { MenuForm } from "../../components/mobile/organisms/MenuForm";
+import { useWishlist } from "../../hooks/useWishlist";
+
+type Props = {
+  ua: string;
+};
+
+const Menu: NextPage<Props> = ({ ua }) => {
+  const device = useMemo(() => {
+    // eslint-disable-next-line
+    return useUserAgent(global.navigator?.userAgent ?? ua);
+  }, [ua]);
+
+  const router = useRouter();
+
+  const id = useMemo(() => {
+    if (typeof router.query.id === "string") {
+      return router.query.id;
+    }
+    return "";
+  }, [router.query.id]);
+
+  const { data, isLoading } = useWishlist();
+
+  const menu = useMemo(() => {
+    if (data) {
+      return data.find((menu) => menu.id === id);
+    }
+  }, [data, id]);
+  return (
+    <>
+      {!device.isMobile ? (
+        <div></div>
+      ) : (
+        <MobileContainer hrefTitle="一覧" href="/wishlist">
+          <main className="h-full w-full flex flex-col overflow-auto">
+            {isLoading ? (
+              <div className="w-full h-full flex justify-center items-center">
+                <Spin tip="loading..." />
+              </div>
+            ) : (
+              menu && (
+                <div className="flex flex-col h-full">
+                  <WeekNavigator
+                    value={new Date()}
+                    onSelect={(date) => {
+                      router.push(`/menus?date=${format(date)}`);
+                    }}
+                  />
+                  <main className="h-full w-full flex flex-col overflow-auto">
+                    <MenuForm menu={menu} date={null} />
+                  </main>
+                </div>
+              )
+            )}
+          </main>
+        </MobileContainer>
+      )}
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const ua = req.headers["user-agent"];
+
+  return {
+    props: {
+      ua,
+    },
+  };
+};
+
+export default Menu;

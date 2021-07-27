a19bd967fe62c0ff8424578e3d6086b29971bffb
agumy <nov13sh@gmail.com>
2021-07-27 22:22:50 +0900

    add desktop signup page

---
+++ src/components/desktop/pages/sign-up.tsx
@@ -0,0 +1,12 @@
+import { DesktopContainer } from "../DesktopContainer";
+import { RegistrationForm } from "../orgnisms/RegistrationForm";
+
+export const SignUp = () => {
+  return (
+    <DesktopContainer>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <RegistrationForm />
+      </div>
+    </DesktopContainer>
+  );
+};

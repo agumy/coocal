aff1fcf7d172112d5429d327d4968ae35e4189bd
agumy <nov13sh@gmail.com>
2021-09-02 21:54:07 +0900

    add mobile signin

---
+++ src/components/mobile/pages/SignIn.tsx
@@ -0,0 +1,9 @@
+import { MobileContainer } from "../containers/MobileContainer";
+
+export const SignIn = () => {
+  return (
+    <MobileContainer>
+      <div className="h-full w-full"></div>
+    </MobileContainer>
+  );
+};
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,8 +1,29 @@
-import { NextPage } from "next";
-import { SignIn } from "../../components/desktop/pages/SignIn";
+import { GetServerSideProps, NextPage } from "next";
+import { useUserAgent } from "next-useragent";
+import { useMemo } from "react";
+import { SignIn as DesktopSignIn } from "../../components/desktop/pages/SignIn";
+import { SignIn as MobileSignIn } from "../../components/mobile/pages/SignIn";
 
-const Login: NextPage = () => {
-  return <SignIn />;
+type Props = {
+  ua: string;
+};
+const SignIn: NextPage<Props> = ({ ua }) => {
+  const device = useMemo(() => {
+    // eslint-disable-next-line
+    return useUserAgent(global.navigator?.userAgent ?? ua);
+  }, [ua]);
+
+  return device.isMobile ? <MobileSignIn /> : <DesktopSignIn />;
 };
 
-export default Login;
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const ua = req.headers["user-agent"];
+
+  return {
+    props: {
+      ua,
+    },
+  };
+};
+
+export default SignIn;

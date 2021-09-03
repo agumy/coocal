5ac3ab1a8a2e9b6bd04c6cb6e5a115db5ca516ae
agumy <nov13sh@gmail.com>
2021-09-03 22:53:44 +0900

    wip for signin

--- src/components/mobile/pages/SignIn.tsx
+++ src/components/mobile/pages/SignIn.tsx
@@ -1,9 +1,23 @@
+import { Button } from "antd";
 import { MobileContainer } from "../containers/MobileContainer";
 
 export const SignIn = () => {
   return (
     <MobileContainer>
-      <div className="h-full w-full"></div>
+      <div className="h-full w-full flex flex-col items-center pt-5">
+        <div className="text-2xl font-bold mb-4 text-gray-600">ログイン</div>
+        <input
+          className="border border-gray-500 mb-4 rounded px-2 py-1 w-56"
+          placeholder="メールアドレス"
+        />
+        <input
+          className="border border-gray-500 mb-4 rounded px-2 py-1 w-56"
+          placeholder="パスワード"
+        />
+        <Button className="w-56 rounded" type="primary">
+          ログイン
+        </Button>
+      </div>
     </MobileContainer>
   );
 };

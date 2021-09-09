f2d95a603fd8d7e9b601ed1d1a6096595760f3ed
agumy <nov13sh@gmail.com>
2021-09-09 21:39:34 +0900

    add login

--- src/components/mobile/pages/SignIn.tsx
+++ src/components/mobile/pages/SignIn.tsx
@@ -1,5 +1,8 @@
 import { Button } from "antd";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 import { useForm } from "react-hook-form";
+import { useSignIn } from "../../../hooks/useSignIn";
 import { MobileContainer } from "../containers/MobileContainer";
 
 export const SignIn = () => {
@@ -11,12 +14,29 @@ export const SignIn = () => {
     reValidateMode: "onBlur",
   });
 
+  const router = useRouter();
+
+  const signIn = useSignIn();
+
+  const submit = useMemo(
+    () =>
+      handleSubmit(async (data) => {
+        await signIn.mutateAsync(data);
+        router.push("/");
+      }),
+    [handleSubmit, router, signIn]
+  );
+
   return (
     <MobileContainer>
-      <div className="h-full w-full flex flex-col items-center pt-5">
+      <form
+        onSubmit={submit}
+        className="h-full w-full flex flex-col items-center pt-5"
+      >
         <div className="text-2xl font-bold mb-4 text-gray-600">ログイン</div>
         <input
           className="border border-gray-500 mb-4 rounded px-2 py-1 w-56"
+          type="text"
           placeholder="メールアドレス"
           {...register("email", {
             required: "メールアドレスを入力してください。",
@@ -24,15 +44,16 @@ export const SignIn = () => {
         />
         <input
           className="border border-gray-500 mb-4 rounded px-2 py-1 w-56"
+          type="password"
           placeholder="パスワード"
           {...register("password", {
             required: "パスワードを入力してください。",
           })}
         />
-        <Button className="w-56 rounded" type="primary">
+        <Button className="w-56 rounded" type="primary" htmlType="submit">
           ログイン
         </Button>
-      </div>
+      </form>
     </MobileContainer>
   );
 };

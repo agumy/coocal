ba19c73b982dbfac3bd7a4a96c729c6ddec4dacd
agumy <nov13sh@gmail.com>
2021-09-11 23:04:09 +0900

    display signin error

--- src/components/mobile/pages/SignIn.tsx
+++ src/components/mobile/pages/SignIn.tsx
@@ -34,22 +34,36 @@ export const SignIn = () => {
         className="h-full w-full flex flex-col items-center pt-5"
       >
         <div className="text-2xl font-bold mb-4 text-gray-600">ログイン</div>
-        <input
-          className="border border-gray-500 mb-4 rounded px-2 py-1 w-56"
-          type="text"
-          placeholder="メールアドレス"
-          {...register("email", {
-            required: "メールアドレスを入力してください。",
-          })}
-        />
-        <input
-          className="border border-gray-500 mb-4 rounded px-2 py-1 w-56"
-          type="password"
-          placeholder="パスワード"
-          {...register("password", {
-            required: "パスワードを入力してください。",
-          })}
-        />
+        <div className="mb-4 flex flex-col items-start">
+          <input
+            className="border border-gray-500 rounded px-2 py-1 w-56"
+            type="text"
+            placeholder="メールアドレス"
+            {...register("email", {
+              required: "メールアドレスを入力してください。",
+            })}
+          />
+          {errors.email && (
+            <div className="text-red-600 text-xs h-3 mt-1">
+              {errors.email.message}
+            </div>
+          )}
+        </div>
+        <div className="mb-4 flex flex-col items-start">
+          <input
+            className="border border-gray-500 rounded px-2 py-1 w-56"
+            type="password"
+            placeholder="パスワード"
+            {...register("password", {
+              required: "パスワードを入力してください。",
+            })}
+          />
+          {errors.password && (
+            <div className="text-red-600 text-xs h-3 mt-1">
+              {errors.password.message}
+            </div>
+          )}
+        </div>
         <Button
           className="w-56 rounded"
           type="primary"

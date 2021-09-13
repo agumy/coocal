491456caea7444924b20cb8cb9dc8394761f4160
agumy <nov13sh@gmail.com>
2021-09-13 20:52:52 +0900

    use login form

--- src/components/mobile/pages/SignIn.tsx
+++ src/components/mobile/pages/SignIn.tsx
@@ -1,78 +1,10 @@
-import { Button } from "antd";
-import { useRouter } from "next/router";
-import { useMemo } from "react";
-import { useForm } from "react-hook-form";
-import { useSignIn } from "../../../hooks/useSignIn";
+import { LoginForm } from "../../desktop/orgnisms/LoginForm";
 import { MobileContainer } from "../containers/MobileContainer";
 
 export const SignIn = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<{ email: string; password: string }>({
-    reValidateMode: "onBlur",
-  });
-
-  const router = useRouter();
-
-  const signIn = useSignIn();
-
-  const submit = useMemo(
-    () =>
-      handleSubmit(async (data) => {
-        await signIn.mutateAsync(data);
-        router.push("/");
-      }),
-    [handleSubmit, router, signIn]
-  );
-
   return (
     <MobileContainer>
-      <form
-        onSubmit={submit}
-        className="h-full w-full flex flex-col items-center pt-5"
-      >
-        <div className="text-2xl font-bold mb-4 text-gray-600">ログイン</div>
-        <div className="mb-4 flex flex-col items-start">
-          <input
-            className="border border-gray-500 rounded px-2 py-1 w-56"
-            type="text"
-            placeholder="メールアドレス"
-            {...register("email", {
-              required: "メールアドレスを入力してください。",
-            })}
-          />
-          {errors.email && (
-            <div className="text-red-600 text-xs h-3 mt-1">
-              {errors.email.message}
-            </div>
-          )}
-        </div>
-        <div className="mb-4 flex flex-col items-start">
-          <input
-            className="border border-gray-500 rounded px-2 py-1 w-56"
-            type="password"
-            placeholder="パスワード"
-            {...register("password", {
-              required: "パスワードを入力してください。",
-            })}
-          />
-          {errors.password && (
-            <div className="text-red-600 text-xs h-3 mt-1">
-              {errors.password.message}
-            </div>
-          )}
-        </div>
-        <Button
-          className="w-56 rounded"
-          type="primary"
-          htmlType="submit"
-          loading={signIn.isLoading}
-        >
-          ログイン
-        </Button>
-      </form>
+      <LoginForm />
     </MobileContainer>
   );
 };
